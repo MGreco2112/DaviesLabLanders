5aa86ff245c857db40f9e62e8bfa95b567e2f587
Matthew Greco <mgreco2112@tutanota.com>
2025-05-05 14:48:01 -0400

    updated HeaderResponses within LanderResponse to include information needed for calculating upload percentages to Frontend

--- src/main/java/com/davies/lab/lander/FormattedModels/ResponseBody/LanderResponse.java
+++ src/main/java/com/davies/lab/lander/FormattedModels/ResponseBody/LanderResponse.java
@@ -2,8 +2,7 @@ package com.davies.lab.lander.FormattedModels.ResponseBody;
 
 import com.davies.lab.lander.Models.*;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.time.LocalDateTime;
 
 public class LanderResponse {
     private String ASDBLanderID;
@@ -20,15 +19,15 @@ public class LanderResponse {
     }
 
     public void createCTDHeadResponse(ProcessedCTDHead head) {
-        CTDHead = new CTDHeadResponse(head.getHeadID());
+        CTDHead = new CTDHeadResponse(head.getHeadID(), head.getBurstTime(), head.getBurstCnt(), head.getStartTime(), head.getEndTime());
     }
 
     public void createDOHeadResponse(ProcessedDOHead head) {
-        DOHead = new DOHeadResponse(head.getHeadID());
+        DOHead = new DOHeadResponse(head.getHeadID(), head.getBurstTime(), head.getBurstCnt(), head.getStartTime(), head.getEndTime());
     }
 
     public void createFLNTUHeadResponse(ProcessedFLNTUHead head) {
-        FLNTUHead = new FLNTUHeadResponse(head.getHeadID());
+        FLNTUHead = new FLNTUHeadResponse(head.getHeadID(), head.getBurstTime(), head.getBurstCnt(), head.getStartTime(), head.getEndTime());
     }
 
     public String getASDBLanderID() {
@@ -81,41 +80,141 @@ public class LanderResponse {
 
     private class CTDHeadResponse {
         private Long HeadID;
+        private Integer burstTime;
+        private Integer burstCnt;
+        private LocalDateTime StartTime;
+        private LocalDateTime EndTime;
 
         public CTDHeadResponse(Long id) {
             HeadID = id;
         }
 
+        public CTDHeadResponse(Long headID, Integer burstTime, Integer burstCnt, LocalDateTime startTime, LocalDateTime endTime) {
+            HeadID = headID;
+            this.burstTime = burstTime;
+            this.burstCnt = burstCnt;
+            StartTime = startTime;
+            EndTime = endTime;
+        }
+
         public Long getHeadID() {
             return HeadID;
         }
 
         public void setHeadID(Long headID) {
             HeadID = headID;
+        }
+
+        public Integer getBurstTime() {
+            return burstTime;
+        }
+
+        public void setBurstTime(Integer burstTime) {
+            this.burstTime = burstTime;
+        }
+
+        public Integer getBurstCnt() {
+            return burstCnt;
+        }
+
+        public void setBurstCnt(Integer burstCnt) {
+            this.burstCnt = burstCnt;
+        }
+
+        public LocalDateTime getStartTime() {
+            return StartTime;
+        }
+
+        public void setStartTime(LocalDateTime startTime) {
+            StartTime = startTime;
+        }
+
+        public LocalDateTime getEndTime() {
+            return EndTime;
+        }
+
+        public void setEndTime(LocalDateTime endTime) {
+            EndTime = endTime;
         }
     }
 
     private class DOHeadResponse {
         private Long HeadID;
+        private Integer BurstTime;
+        private Integer BurstCnt;
+        private LocalDateTime StartTime;
+        private LocalDateTime endTime;
 
         public DOHeadResponse(Long id) {
             HeadID = id;
         }
 
+        public DOHeadResponse(Long headID, Integer burstTime, Integer burstCnt, LocalDateTime startTime, LocalDateTime endTime) {
+            HeadID = headID;
+            BurstTime = burstTime;
+            BurstCnt = burstCnt;
+            StartTime = startTime;
+            this.endTime = endTime;
+        }
+
         public Long getHeadID() {
             return HeadID;
         }
 
         public void setHeadID(Long headID) {
             HeadID = headID;
+        }
+
+        public Integer getBurstTime() {
+            return BurstTime;
+        }
+
+        public void setBurstTime(Integer burstTime) {
+            BurstTime = burstTime;
+        }
+
+        public Integer getBurstCnt() {
+            return BurstCnt;
+        }
+
+        public void setBurstCnt(Integer burstCnt) {
+            BurstCnt = burstCnt;
+        }
+
+        public LocalDateTime getStartTime() {
+            return StartTime;
+        }
+
+        public void setStartTime(LocalDateTime startTime) {
+            StartTime = startTime;
+        }
+
+        public LocalDateTime getEndTime() {
+            return endTime;
+        }
+
+        public void setEndTime(LocalDateTime endTime) {
+            this.endTime = endTime;
         }
     }
 
     private class FLNTUHeadResponse {
         private Long HeadID;
+        private Integer BurstTime;
+        private Integer BurstCnt;
+        private LocalDateTime StartTime;
+        private LocalDateTime EndTime;
 
         public FLNTUHeadResponse(Long id) {
             HeadID = id;
+        }
+
+        public FLNTUHeadResponse(Long headID, Integer burstTime, Integer burstCnt, LocalDateTime startTime, LocalDateTime endTime) {
+            HeadID = headID;
+            BurstTime = burstTime;
+            BurstCnt = burstCnt;
+            StartTime = startTime;
+            EndTime = endTime;
         }
 
         public Long getHeadID() {
@@ -124,6 +223,38 @@ public class LanderResponse {
 
         public void setHeadID(Long headID) {
             HeadID = headID;
+        }
+
+        public Integer getBurstTime() {
+            return BurstTime;
+        }
+
+        public void setBurstTime(Integer burstTime) {
+            BurstTime = burstTime;
+        }
+
+        public Integer getBurstCnt() {
+            return BurstCnt;
+        }
+
+        public void setBurstCnt(Integer burstCnt) {
+            BurstCnt = burstCnt;
+        }
+
+        public LocalDateTime getStartTime() {
+            return StartTime;
+        }
+
+        public void setStartTime(LocalDateTime startTime) {
+            StartTime = startTime;
+        }
+
+        public LocalDateTime getEndTime() {
+            return EndTime;
+        }
+
+        public void setEndTime(LocalDateTime endTime) {
+            EndTime = endTime;
         }
     }
 
