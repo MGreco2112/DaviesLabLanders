0e2749389b62f42d7d6df295818512bdacbe7cd5
Matthew Greco <mgreco2112@tutanota.com>
2025-08-01 12:59:30 -0400

    enabled cacheing

--- src/main/java/com/davies/lab/lander/Controllers/DashboardController.java
+++ src/main/java/com/davies/lab/lander/Controllers/DashboardController.java
@@ -6,6 +6,7 @@ import com.davies.lab.lander.Models.Lander;
 import com.davies.lab.lander.Repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -19,6 +20,7 @@ import java.util.*;
 @CrossOrigin
 @RestController
 @RequestMapping("/api/dashboard")
+@EnableCaching
 public class DashboardController {
     @Autowired
     private LanderRepository landerRepository;
